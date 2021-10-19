79913c831a7f8144ba1e7ffe380665304202b41a
stepanshvets <sh_stepa@mail.ru>
2021-10-19 23:43:40 +0300

    Add to count time

--- Array/Main.cpp
+++ Array/Main.cpp
@@ -15,9 +15,17 @@ int main()
     C.Show();
     D.Show();
 
+    /*
+    clock_t t = clock();
+    for (int i = 0; i < 1000000; i++) Set R = A & B & C & D;
+    t = clock() - t;
+    cout << "Time: " << t / (double)CLOCKS_PER_SEC << endl;
+    */
+
     E = A & B & C & D;
     cout << endl;
     cout << "A & B & C & D:\n";
     E.Show();
+
     return 0;
 }
