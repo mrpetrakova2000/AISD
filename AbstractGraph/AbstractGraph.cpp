2bd8dfe4f9b332ba1553607e833b4f7d4cb7ee2b
Марина Петракова <mrpetrakova2000@gmail.com>
2021-11-30 11:29:50 +0300

    graph

--- AbstractGraph/AbstractGraph.cpp
+++ AbstractGraph/AbstractGraph.cpp
@@ -1,30 +1,103 @@
 ﻿#include <iostream>
 #include <vector>
 #include <list>
+#include <fstream>
+#include <string>
+
+const int inf = 1000000;
 
 class AbstractGraph {
 private:
     int n, m;
     std::vector<std::vector<int>> MatrixAdj;
 public:
-    void Input() {
-        std::cin >> n >> m;
 
+    AbstractGraph() {
+        Input();
+    }
+
+    AbstractGraph(int n): n(n), m(rand()%(n*(n-1)/2)) {
         MatrixAdj.resize(n);
-        for (int i = 0; i < n; i++) MatrixAdj[i].resize(n);
+        for (int i = 0; i < n; i++) MatrixAdj[i].resize(n, inf);
 
-        for (int i = 0; i < m; i++) {
-            int v, u, s;
-            std::cin >> v >> u >> s;
-            MatrixAdj[v - 1][u - 1] = MatrixAdj[u - 1][v - 1] = s;
+        Generation(n, m);
+    };
+
+    void Input() {
+        int choice;
+        std::string fname;
+        std::cout << "Do you want to read data from file or write by console? Enter 0 or 1" << std::endl;
+        std::cin >> choice;
+        if (!choice) {
+            std::ifstream F;
+            std::cout << "Enter filename: ";
+            std::cin >> fname;
+            F.open(fname, std::ios::in);
+            F >> n >> m;
+
+            MatrixAdj.resize(n);
+            for (int i = 0; i < n; i++) MatrixAdj[i].resize(n);
+
+            for (int i = 0; i < m; i++) {
+                int v, u, s;
+                F >> v >> u >> s;
+                MatrixAdj[v - 1][u - 1] = MatrixAdj[u - 1][v - 1] = s;
+            }
+            F.close();
+        }
+        else {
+            std::cout << "Enter count of vertices and edges: ";
+            std::cin >> n >> m;
+
+            MatrixAdj.resize(n);
+            for (int i = 0; i < n; i++) MatrixAdj[i].resize(n);
+
+            std::cout << "Enter numbers of start and final vertices of edges and lengths of the edges:\n";
+            for (int i = 0; i < m; i++) {
+                int v, u, s;
+                std::cin >> v >> u >> s;
+                MatrixAdj[v - 1][u - 1] = MatrixAdj[u - 1][v - 1] = s;
+            }
+        }
+
+    }
+
+    void Generation(int n, int m) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (rand() % 2){
+                    int a = rand() % 100;
+                    MatrixAdj[i][j] = a;
+                    MatrixAdj[j][i] = a;
+                } 
+            }
+            MatrixAdj[i][i] = 0;
         }
     }
 
     void Output() {
+        std::cout << "\n  | ";
+        for (int i = 0; i < n; ++i) {
+            std::cout.width(3);
+            std::cout << i + 1 << " ";
+        }
         std::cout << std::endl;
-        for (auto& i : MatrixAdj) {
-            for (auto& j : i)
-                std::cout << j << " ";
+
+        for (int i = 0; i < 4*n + 3; ++i) std::cout << "-";
+        std::cout << std::endl;
+
+        for (int i = 0; i < n; ++i) {
+            std::cout << i + 1 << " | ";
+            for (int j = 0; j < n; ++j) {
+                if (MatrixAdj[i][j] == inf) {
+                    std::cout.width(3);
+                    std::cout << "inf" << " ";
+                }
+                else {
+                    std::cout.width(3);
+                    std::cout << MatrixAdj[i][j] << " ";
+                }
+            }
             std::cout << std::endl;
         }
     }
@@ -34,22 +107,26 @@ public:
 };
 
 int main() {
+    srand(time(0));
+    //AbstractGraph G;
+    AbstractGraph G1(7);
 
-    AbstractGraph G;
+    //G.Output();
+    G1.Output();
 
-    G.Input();
-    G.Output();
-
-    int s = 1, f = 6;
+    int s, f;
+    std::cout << "\nEnter numbers of start and final vertices: ";
+    
+    std::cin >> s >> f;
 
     std::cout << "Distance: ";
-    std::vector<int> A = Dijkstra(G, s - 1);
+    std::vector<int> A = Dijkstra(G1, s - 1);
     for (auto& i : A)
         std::cout << i << " ";
     std::cout << std::endl;
 
     std::cout << "Path: ";
-    std::list<int> B = Path(G, A, s - 1, f - 1);
+    std::list<int> B = Path(G1, A, s - 1, f - 1);
     for (auto& i : B)
         std::cout << i << " ";
 
@@ -70,7 +147,7 @@ std::vector<int> Dijkstra(AbstractGraph G, int s) {
     visited[s] = 1;
 
     for (int i = 0; i < G.n; i++) {
-        int v, minWeight = 1000000;
+        int v = s, minWeight = 1000000;
 
         for (int j = 0; j < G.n; j++)
             if (not visited[j] and D[j] < minWeight) {
